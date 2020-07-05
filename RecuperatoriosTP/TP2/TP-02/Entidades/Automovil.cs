d81827cc8a1220f8656bfbd1dc9ff9680fa2d77d
Molinari <julieta.molinari@accenture.com>
2020-07-05 19:59:21 -0300

    Recuperatorio TP2

---
+++ RecuperatoriosTP/TP2/TP-02/Entidades/Automovil.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Entidades
+{
+    public class Automovil : Vehiculo
+    {
+        private ETipo tipo;
+        public enum ETipo { Monovolumen, Sedan }
+        
+
+        /// <summary>
+        /// Constructor de automovil: Por defecto, TIPO será Monovolumen
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Automovil(EMarca marca, string chasis, ConsoleColor color)
+            : base(chasis, marca, color)
+        {
+            this.tipo = ETipo.Monovolumen;
+        }
+
+        /// <summary>
+        /// Constructor con todos sus parametros
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Automovil(EMarca marca, string chasis, ConsoleColor color,ETipo tipo)
+            : this(marca,chasis,color)
+        {
+            this.tipo = tipo;
+        }
+
+        /// <summary>
+        /// Los automoviles son medianos
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return Automovil.ETamanio.Mediano;
+            }
+        }
+
+        /// <summary>
+        ///  Metodo mostrar, imprime datos del Automovil
+        /// </summary>
+        /// <returns></returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("AUTOMOVIL");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("TAMAÑO : {0}", this.Tamanio);
+            sb.AppendLine("TIPO : " + this.tipo);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ RecuperatoriosTP/TP2/TP-02/Entidades/Moto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Moto : Vehiculo
+    {        
+        
+        /// <summary>
+        ///Propiedad del tamaño: Las motos son chicas
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return Moto.ETamanio.Chico;
+            }
+        }
+
+        /// <summary>
+        /// Constructor de moto
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="codigo"></param>
+        /// <param name="color"></param>
+        public Moto(EMarca marca, string codigo, ConsoleColor color) : base(codigo, marca, color)
+        {
+        }
+
+        /// <summary>
+        /// Metodo mostrar, imprime datos de la moto
+        /// </summary>
+        /// <returns></returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("MOTO");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("TAMAÑO : {0}", this.Tamanio);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
